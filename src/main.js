882b5b1a351aa334c7e107fb6152bd524e996ec0
huks_wno1@163.com <huks_wno1@163.com>
2022-04-17 21:04:54 +0800

    bim模型加载

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import Cookies from 'js-cookie'
 
 import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+import '@arcgis/core/assets/esri/themes/light/main.css'
 
 import Element from 'element-ui'
 import './styles/element-variables.scss'
